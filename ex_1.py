52135995691f8d1a42e0daf80ce6c01fc8cba485
GriDimVal <93321401+GriDimVal@users.noreply.github.com>
2022-03-29 12:52:48 +0300

    ДЗ по уроку 3
    Объединил задание 6 и 7 в одном файле, как сказано в методичке

---
+++ ex_1.py
@@ -0,0 +1,11 @@
+def div(s_1, s_2):
+    try:
+        s_1, s_2 = int(s_1), int(s_2)
+        div_num = s_1 / s_2
+    except ValueError:
+        return 'Некорректное значение данных'
+    except ZeroDivisionError:
+        return 'Делить на ноль - нельзя!'
+    return round(div_num, 4)
+
+print(div(input('Введите первое число - '), input('Введите второе число - ')))
