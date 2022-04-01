7e0bf67229eac33baf1c29a25508830164c0b8c2
GriDimVal <93321401+GriDimVal@users.noreply.github.com>
2022-04-01 22:57:16 +0300

    Add files via upload

---
+++ ex_2.py
@@ -0,0 +1,7 @@
+from random import randint
+
+original_list = [randint(0, 1000) for _ in range(0, randint(2, 20))]
+answer_list = [num for i, num in enumerate(original_list[1:]) if num > original_list[i]]
+
+print(original_list)
+print(answer_list)
